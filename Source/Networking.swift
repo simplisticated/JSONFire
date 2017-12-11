9226f2134c08f82b63f4c29026d929771fbb788d
Igor Matyushkin <igormatyushkin014@gmail.com>
2017-12-12 00:27:59 +0300

    Created Networking.Services structure as basic architecture solution for third-party apps 

---
+++ Source/Networking.swift
@@ -0,0 +1,16 @@
+//
+//  Networking.swift
+//  JSONFire
+//
+//  Created by Igor Matyushkin on 12.12.2017.
+//  Copyright © 2017 Igor Matyushkin. All rights reserved.
+//
+
+import Foundation
+
+public struct Networking {
+    
+    public struct Services {
+    }
+    
+}
