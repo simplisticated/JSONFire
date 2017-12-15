e80c990faddae6abb5ea96f9e53d80ab6daadf5b
Igor Matyushkin <igormatyushkin014@gmail.com>
2017-12-16 00:33:41 +0300

    Added ResponseDelay type 

---
+++ Source/ResponseDelay.swift
@@ -0,0 +1,15 @@
+
+//
+//  ResponseDelay.swift
+//  JSONFire
+//
+//  Created by Igor Matyushkin on 15.12.2017.
+//  Copyright © 2017 Igor Matyushkin. All rights reserved.
+//
+
+import Foundation
+
+public enum ResponseDelay {
+    case seconds(count: Int)
+    case none
+}
